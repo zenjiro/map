d343e06920008e35fac0566894f7fe5e6843ffe8
zenjiro <zenjiro0123@gmail.com>
2007-01-03 07:56:57 +0000

    最短経路探索を実装しはじめました。まだ動きません。

--- ksj/ConvertRoadToCSV.java
+++ ksj/ConvertRoadToCSV.java
@@ -58,25 +58,20 @@ public class ConvertRoadToCSV {
 	 * @throws UnsupportedEncodingException サポート外エンコーディング例外
 	 * @throws FileNotFoundException ファイル未検出例外
 	 */
-	public static void main(final String[] args)
-			throws UnsupportedEncodingException, FileNotFoundException {
+	public static void main(final String[] args) throws UnsupportedEncodingException, FileNotFoundException {
 		final Map<String, String> attributes = new HashMap<String, String>();
 		{
 			final File file = new File("N01_07L_dl.txt");
-			final Scanner scanner = new Scanner(new InputStreamReader(
-					new FileInputStream(file), "SJIS"));
+			final Scanner scanner = new Scanner(new InputStreamReader(new FileInputStream(file), "SJIS"));
 			while (scanner.hasNextLine()) {
 				final String line = scanner.nextLine();
 				if (line.startsWith("DL")) {
 					final String attributeNumber = line.substring(3, 14).trim();
 					final String[] items = line.substring(18).split(" ");
-					final String attribute = (items[0] + (items.length > 1 ? items[1]
-							: "")).trim().replace(" ", "").replace("０", "0")
-							.replace("１", "1").replace("２", "2").replace("３",
-									"3").replace("４", "4").replace("５", "5")
-							.replace("６", "6").replace("７", "7").replace("８",
-									"8").replace("９", "9").replaceFirst("号線$",
-									"号");
+					final String attribute = (items[0] + (items.length > 1 ? items[1] : "")).trim().replace(" ", "")
+							.replace("０", "0").replace("１", "1").replace("２", "2").replace("３", "3").replace("４", "4")
+							.replace("５", "5").replace("６", "6").replace("７", "7").replace("８", "8").replace("９", "9")
+							.replaceFirst("号線$", "号");
 					attributes.put(attributeNumber, attribute);
 				}
 			}
@@ -90,44 +85,30 @@ public class ConvertRoadToCSV {
 				System.out.println(file);
 				final Map<String, Link> links = new HashMap<String, Link>();
 				{
-					final Scanner scanner = new Scanner(new InputStreamReader(
-							new FileInputStream(file), "SJIS"));
+					final Scanner scanner = new Scanner(new InputStreamReader(new FileInputStream(file), "SJIS"));
 					boolean isLink = false;
 					String lastLink = null;
 					while (scanner.hasNextLine()) {
 						final String line = scanner.nextLine();
 						if (line.startsWith("L")) {
 							final String meshCode = line.substring(3, 9).trim();
-							final String linkNumber = line.substring(27, 33)
-									.trim();
-							final String attributeNumber = line.substring(35,
-									45).trim();
+							final String linkNumber = line.substring(27, 33).trim();
+							final String attributeNumber = line.substring(35, 45).trim();
 							if (attributes.containsKey(attributeNumber)) {
-								final String attribute = attributes
-										.get(attributeNumber);
-								links.put(meshCode + "_" + linkNumber,
-										new Link(new ArrayList<Point2D>(),
-												attribute));
+								final String attribute = attributes.get(attributeNumber);
+								links.put(meshCode + "_" + linkNumber, new Link(new ArrayList<Point2D>(), attribute));
 								lastLink = meshCode + "_" + linkNumber;
 							}
 							isLink = true;
 						} else {
 							if (isLink) {
 								if (lastLink != null) {
-									final String[] items = line.trim().split(
-											"[ \t]+");
+									final String[] items = line.trim().split("[ \t]+");
 									for (int i = 1; i < items.length; i += 2) {
-										final int x = Integer
-												.parseInt(items[i - 1]);
-										final int y = Integer
-												.parseInt(items[i]);
-										Point2D point = UTMUtil
-												.toUTM(new Point2D.Double(
-														x / 36000.0,
-														y / 36000.0));
-										links.get(lastLink).points
-												.add(new Point2D.Double(point
-														.getX(), -point.getY()));
+										final int x = Integer.parseInt(items[i - 1]);
+										final int y = Integer.parseInt(items[i]);
+										Point2D point = UTMUtil.toUTM(new Point2D.Double(x / 36000.0, y / 36000.0));
+										links.get(lastLink).points.add(new Point2D.Double(point.getX(), -point.getY()));
 									}
 								}
 							}
@@ -147,12 +128,10 @@ public class ConvertRoadToCSV {
 						for (final Point2D point : link.points) {
 							if (path == null) {
 								path = new GeneralPath();
-								path.moveTo((float) point.getX(), (float) point
-										.getY());
+								path.moveTo((float) point.getX(), (float) point.getY());
 								start = point;
 							} else {
-								path.lineTo((float) point.getX(), (float) point
-										.getY());
+								path.lineTo((float) point.getX(), (float) point.getY());
 								end = point;
 							}
 						}
@@ -160,8 +139,10 @@ public class ConvertRoadToCSV {
 						simpleShapes.put(new Line2D.Double(start, end), link.attribute);
 					}
 				}
-				ShapeIO.writeShape(shapes, new FileOutputStream(new File(Const.Ksj.ROAD_FINE_PREFIX + matcher.group(1) + Const.Ksj.ROAD_SUFFIX)));
-				ShapeIO.writeShape(simpleShapes, new FileOutputStream(new File(Const.Ksj.ROAD_SIMPLE_PREFIX + matcher.group(1) + Const.Ksj.ROAD_SUFFIX)));
+				ShapeIO.writeShape(shapes, new FileOutputStream(new File(Const.Ksj.ROAD_FINE_PREFIX + matcher.group(1)
+						+ Const.Ksj.ROAD_SUFFIX)));
+				ShapeIO.writeShape(simpleShapes, new FileOutputStream(new File(Const.Ksj.ROAD_SIMPLE_PREFIX
+						+ matcher.group(1) + Const.Ksj.ROAD_SUFFIX)));
 			}
 		}
 	}
